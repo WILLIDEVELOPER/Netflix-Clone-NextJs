cdc3a6f160bee362ce483499c9a399c9b5784734
William Home <wilsai1023@gmail.com>
2023-05-14 17:21:15 -0500

    step four

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,36 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
   return (
     <>
-      <h1>Netflix clone</h1>
+      <h1 className="text-4xl text-white font-bold">Netflix clone</h1>
+      <p className="text-white">Logged in as: {user?.name}</p>
+      <button
+        className="text-white font-bold w-full h-10 bg-slate-700"
+        onClick={() => signOut()}
+      >
+        logout
+      </button>
     </>
   );
 }
